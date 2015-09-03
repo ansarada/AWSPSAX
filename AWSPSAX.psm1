031942c43c975a701037821e806606f4cac13684
ebh <alex.dess@ansarada.com>
2015-09-04 09:54:34 +1000

    Made exported member list dynamic

--- AWSPSAX.psm1
+++ AWSPSAX.psm1
@@ -5,6 +5,7 @@ $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 "$moduleRoot\Functions\*.ps1" |
 Resolve-Path |
 Where-Object { -not ($_.ProviderPath.Contains(".Tests.")) } |
-ForEach-Object { . $_.ProviderPath }
-
-Export-ModuleMember Compress-S3, Get-MyAWSRegion, Invoke-SqlBackupToS3
+ForEach-Object {
+	. $_.ProviderPath
+	Export-ModuleMember $(Split-Path -Leaf $_.ProviderPath).Replace('.ps1', '')
+}
