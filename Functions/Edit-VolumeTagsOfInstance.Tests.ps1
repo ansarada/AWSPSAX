c4b0828e1cf6d0ab95fa63e0390730471471241f
Daniel Tan <daniel.tan@ansarada.com>
2016-07-25 11:35:54 +1000

    Added 2 new functions Edit-VolumeTagsOfInstnaces to replace Edit-VolimeTagsOfInstances in the future

---
+++ Functions/Edit-VolumeTagsOfInstance.Tests.ps1
@@ -0,0 +1,291 @@
+﻿#requires -Version 3 -Modules AWSPowerShell
+
+$currentDirectory = Split-Path -Parent $PSCommandPath
+$sourceFile = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
+. "$currentDirectory\$sourceFile"
+
+
+Describe "Edit-VolumeTagsOfInstance" {
+
+    Context "Testing parameters validation" {
+
+        $emptyArray = @('')
+        $fakeArray = @('a')
+        Write-Host "Context initialisation completed"
+        
+         It "Passing mandatory parameter null and empty should throw exception" {
+
+            { Edit-VolumeTagsOfInstance -InstanceID $null -Tags $null } | Should Throw
+            { Edit-VolumeTagsOfInstance -InstanceID $null -Tags $fakeArray } | Should Throw
+            { Edit-VolumeTagsOfInstance -InstanceID '' -Tags $fakeArray } | Should Throw
+            { Edit-VolumeTagsOfInstance -InstanceID 'abcd' -Tags $emptyArray } | Should Throw
+            
+        }
+
+    }
+
+    Context "Testing different situations when both parameters are passed" {
+
+        $fakeInstanceID = 'abcd'
+        $fakeTags = @('a')
+        Write-Host "Context initialisation completed"
+
+        It "When Get-EC2InstanceAttribute throws exception this method should throw exception" {
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {throw}
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -Tags $fakeTags } | Should Throw
+            
+        }
+
+         It "When Get-EC2InstanceAttribute returns null this method should throw exception" {
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$null}
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -Tags $fakeTags } | Should Throw
+            
+        }
+
+        It "When Get-EC2InstanceAttribute returns an empty image object this method should not throw exception" {
+
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+            Mock -CommandName New-EC2Tag -MockWith {$null}
+            
+            $new_EC2Tag_Call_Counter = 0
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -tags $fakeTags} | Should not Throw
+
+            Assert-MockCalled -CommandName New-EC2Tag -time $new_EC2Tag_Call_Counter -Scope It -Exactly  
+        }
+
+        It "When Get-EC2InstanceAttribute returns a propoerly mocked ec2 object which contains 1 ebs volume. this method should call AWS tag methods 1 time only" {
+
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            $fakeVolume = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume.VolumeId = "test123"
+
+            $fakeBlockDeviceMapping = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping.Ebs = $fakeVolume
+
+            $fakeInstanceAttribute.BlockDeviceMappings = $fakeBlockDeviceMapping
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+            Mock -CommandName New-EC2Tag -MockWith {$null}
+            
+            $new_EC2Tag_Call_Counter = 1
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -tags $fakeTags} | Should not Throw
+
+            Assert-MockCalled -CommandName New-EC2Tag -time $new_EC2Tag_Call_Counter -Scope It -Exactly
+
+        }
+
+        It "When Get-EC2InstanceAttribute returns a propoerly mocked ec2 object which contains 2 ebs volumes. this method should call AWS tag methods 2 time only" {
+
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            $fakeVolume1 = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume1.VolumeId = "test123"
+            $fakeVolume2 = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume2.VolumeId = "test456"
+
+            $fakeBlockDeviceMapping1 = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping1.Ebs = $fakeVolume1
+            $fakeBlockDeviceMapping2 = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping2.Ebs = $fakeVolume2
+
+            $fakeInstanceAttribute.BlockDeviceMappings = @($fakeBlockDeviceMapping1, $fakeBlockDeviceMapping2)
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+            Mock -CommandName New-EC2Tag -MockWith {$null}
+            
+            $new_EC2Tag_Call_Counter = 2
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -tags $fakeTags} | Should not Throw
+
+            Assert-MockCalled -CommandName New-EC2Tag -time $new_EC2Tag_Call_Counter -Scope It -Exactly
+
+        }
+
+    }
+
+    context "If passed array is not of type [ Amazon.EC2.Model.Tag] then the method returns error" {
+
+        $fakeInstanceID = 'abcd'
+        $fakeTags = @('a')
+        Write-Host "Context initialisation completed"
+        
+        It "If passed array is not of type [ Amazon.EC2.Model.Tag] then the method returns error" {
+        
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            $fakeVolume = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume.VolumeId = "vol-test1111"
+
+            $fakeBlockDeviceMapping = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping.Ebs = $fakeVolume
+
+            $fakeInstanceAttribute.BlockDeviceMappings = $fakeBlockDeviceMapping
+
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+
+            { Edit-VolumeTagsOfInstance -InstanceID $fakeInstanceID -tags $fakeTags} | Should Throw
+
+        }    
+
+    }
+
+    Context "Testing different situations when both parameters are not passed"{
+
+        It "When invoke-restmethod throws exception this method should throw exception" {
+
+             Mock -CommandName invoke-restmethod -MockWith {throw}
+
+            { Edit-VolumeTagsOfInstance } | Should Throw
+            
+
+        }
+
+        It "When invoke-restmethod returns null this method should throw exception" {
+
+             Mock -CommandName invoke-restmethod -MockWith {$null}
+
+            { Edit-VolumeTagsOfInstance } | Should Throw 
+
+        }
+
+        It "When Get-EC2Tag throws exception this method should throw exception" {
+
+             Mock -CommandName invoke-restmethod -MockWith {"i-12345"}
+             Mock -CommandName Get-EC2Tag -MockWith {throws}
+
+            { Edit-VolumeTagsOfInstance } | Should Throw 
+
+        }
+
+        It "When Get-EC2Tag returns null this method should not throw exception and gracefully returns" {
+
+             Mock -CommandName invoke-restmethod -MockWith {"i-12345"}
+             Mock -CommandName Get-EC2Tag -MockWith {$null}
+             Mock -CommandName Get-EC2InstanceAttribute -MockWith {$null}
+
+             $get_EC2InstanceAttribute_Call_Counter = 0
+
+            { Edit-VolumeTagsOfInstance } | Should not Throw 
+
+             Assert-MockCalled -CommandName Get-EC2InstanceAttribute -time $get_EC2InstanceAttribute_Call_Counter -Scope It -Exactly
+
+             # this test case also tests that if any case, $actualTags contains no tags then this happens
+
+        }
+
+        It "When Get-EC2Tag returns tags that only contains keys starting with 'aws' this method should not throw exception and gracefully returns" {
+
+            $testTags = @(
+                @{
+                    Key   = 'awsName'
+                    Value = "test"
+                },
+                @{
+                    Key   = 'awsApplication'
+                    Value = 'DataRoom'
+                }
+            )
+
+            Mock -CommandName invoke-restmethod -MockWith {"i-12345"}
+            Mock -CommandName Get-EC2Tag -MockWith {$testTags}
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$null}
+
+            $get_EC2InstanceAttribute_Call_Counter = 0
+
+            { Edit-VolumeTagsOfInstance } | Should not Throw 
+
+            Assert-MockCalled -CommandName Get-EC2InstanceAttribute -time $get_EC2InstanceAttribute_Call_Counter -Scope It -Exactly
+
+
+        }
+
+        It "When Get-EC2InstanceAttribute returns a propoerly mocked ec2 object which contains 1 ebs volume. this method should call AWS tag methods 1 time only" {
+
+            $testTags = @(
+                @{
+                    Key   = 'Name'
+                    Value = "test"
+                },
+                @{
+                    Key   = 'Application'
+                    Value = 'DataRoom'
+                }
+            )
+
+
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            $fakeVolume = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume.VolumeId = "test123"
+
+            $fakeBlockDeviceMapping = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping.Ebs = $fakeVolume
+
+            $fakeInstanceAttribute.BlockDeviceMappings = $fakeBlockDeviceMapping
+
+            Mock -CommandName invoke-restmethod -MockWith {"i-12345"}
+            Mock -CommandName Get-EC2Tag -MockWith {$testTags}
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+            Mock -CommandName New-EC2Tag -MockWith {$null}
+            
+            $new_EC2Tag_Call_Counter = 1
+
+            { Edit-VolumeTagsOfInstance} | Should not Throw
+
+            Assert-MockCalled -CommandName New-EC2Tag -time $new_EC2Tag_Call_Counter -Scope It -Exactly
+
+        }
+       
+        It "When Get-EC2InstanceAttribute returns a propoerly mocked ec2 object which contains 2 ebs volume. this method should call AWS tag methods 2 time only" {
+
+            $testTags = @(
+                @{
+                    Key   = 'Name'
+                    Value = "test"
+                },
+                @{
+                    Key   = 'Application'
+                    Value = 'DataRoom'
+                }
+            )
+
+
+            $fakeInstanceAttribute = New-Object -TypeName Amazon.EC2.Model.InstanceAttribute
+
+            $fakeVolume1 = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume1.VolumeId = "test123"
+            $fakeVolume2 = New-Object Amazon.EC2.Model.EbsInstanceBlockDevice
+            $fakeVolume2.VolumeId = "test456"
+
+            $fakeBlockDeviceMapping1 = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping1.Ebs = $fakeVolume1
+            $fakeBlockDeviceMapping2 = New-Object -TypeName Amazon.EC2.Model.InstanceBlockDeviceMapping
+            $fakeBlockDeviceMapping2.Ebs = $fakeVolume2
+
+            $fakeInstanceAttribute.BlockDeviceMappings = @($fakeBlockDeviceMapping1, $fakeBlockDeviceMapping2)
+
+            Mock -CommandName invoke-restmethod -MockWith {"i-12345"}
+            Mock -CommandName Get-EC2Tag -MockWith {$testTags}
+            Mock -CommandName Get-EC2InstanceAttribute -MockWith {$fakeInstanceAttribute}
+            Mock -CommandName New-EC2Tag -MockWith {$null}
+            
+            $new_EC2Tag_Call_Counter = 2
+
+            { Edit-VolumeTagsOfInstance} | Should not Throw
+
+            Assert-MockCalled -CommandName New-EC2Tag -time $new_EC2Tag_Call_Counter -Scope It -Exactly
+
+        }
+
+
+    }
+}
---
+++ Functions/Edit-VolumeTagsOfInstance.ps1
@@ -0,0 +1,110 @@
+﻿<#
+.Synopsis
+   Adds set of tags to volumes of an EC2
+.DESCRIPTION
+   This function first locates all attached volumes of an EC2. And then applies a set of tags to each. Any Tag key that starts
+   with aws is ignored.
+.EXAMPLE
+   When both parameters are passed,
+   $testTags = @(
+                @{
+                    Key   = 'Name'
+                    Value = "test"
+                },
+                @{
+                    Key   = 'Application'
+                    Value = 'DataRoom'
+                }
+            )
+    Edit-VolumeTagsOfInstance -InstanceID "i-123445" -Tags $testTags
+
+    or simply calling the method in an ec2,
+    Edit-VolumeTagsOfInstance 
+#>
+function Edit-VolumeTagsOfInstance
+{
+    [CmdletBinding( PositionalBinding = $false )]
+    Param
+    (
+        # If an instance ID is passed then tags are applied to that particular instances' volumes. Otherwise, this method expects
+        # the executing environment is an EC2. Then this method applies tags to this instance's volumes. 
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $InstanceID,
+
+        # A array containing key and value of each tag. If this parameter is passed then the method applies these tags.
+        # If this parameter is not passed then this method expects that the executing environment is an EC2 and tags
+        # associated with this current ec2 will be applied.
+        [ValidateNotNullOrEmpty()]
+        [Array]
+        $Tags
+    )
+
+    $actualInstanceID = [string]::Empty
+    $actualTags = $null
+
+    if ($InstanceID)
+    {
+        $actualInstanceID = $InstanceID
+    }
+    else
+    {
+        $actualInstanceID = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
+    }
+
+    if ([string]::IsNullOrEmpty($actualInstanceID))
+    {
+        throw "The actual instance ID could not be determined"
+    }
+
+    Write-host "Updating volume tags of instnace $actualInstanceID"
+
+    if ($Tags)
+    {
+        $actualTags = $Tags
+    }
+    else
+    {
+        $currentEC2Tags = Get-EC2Tag -Filter @{ Name="resource-id";Values=$actualInstanceID} 
+        $listedTags = $currentEC2Tags | Where {$_.key -notlike "aws*"} 
+	
+	    $actualTags = @()
+
+	    $listedTags | where {$_} | foreach {
+	        $tag = @{
+        	    Key   = $_.key
+        	    Value = $_.Value
+    	    }
+
+	    $actualTags += $tag
+
+	    }
+    }
+
+    if ($actualTags.Count -le 0)
+    {
+        write-host "No tag was identified for update. Method returns now."
+        return
+    }
+
+    # Get the volumes of the ec2
+    $instanceAttributes = Get-EC2InstanceAttribute -InstanceId $actualInstanceID -Attribute "BlockDeviceMapping"
+
+    if ($instanceAttributes -isnot [Amazon.EC2.Model.InstanceAttribute])
+    {
+        throw "Get-EC2InstanceAttribute did not return Amazon.EC2.Model.InstanceAttribute object"
+    }
+
+    $ec2BlockDeviceMapping = $instanceAttributes.BlockDeviceMappings
+    $ebsDevices = $ec2BlockDeviceMapping | where {$_.EBS}
+
+    $ebsDevices | ForEach-Object -Process {
+        
+        $volumeId = $_.Ebs.VolumeId
+        
+        Write-host "Updating tags for volume $volumeId"
+
+        New-EC2Tag -Resources $volumeId -Tags $actualTags 
+    }# Add tags to volumes associated with the EC2 using Amazon.EC2.Model.EbsInstanceBlockDevice 
+
+}
